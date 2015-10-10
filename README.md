39657d61fc63e73b68864c050c7e4773c12eb8df
Antoine Patel <antoine.patel.fr@gmail.com>
2015-10-10 22:27:33 +0200

    Update README: zenity, wmctrl, licence, install.

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # xwindow-switcher.sh
+
 A minimalist (<20 LOC) x11 window switcher (ALT+TAB replacement) in BASH.
 
+Powered by [wmctrl](https://sites.google.com/site/tstyblo/wmctrl/) and
+[Zenity](https://wiki.gnome.org/Projects/Zenity).
+
+License: [MIT](LICENSE)
+
 ![xwindow-switcher screenshot](screenshot.png)
+
+## Installation
+
+1. Make sure you have
+[wmctrl](https://sites.google.com/site/tstyblo/wmctrl/) and
+[Zenity](https://wiki.gnome.org/Projects/Zenity) installed first (Use
+you package manager if not).
+
+2. Run [xwindow-switcher.sh](xwindow-switcher.sh) in a terminal
+   (`./xwindow-switcher.sh`).
--- xwindow-switcher.sh
+++ xwindow-switcher.sh
@@ -2,8 +2,7 @@
 
 # Antoine Patel (antoine.patel.fr@gmail.com)
 #
-# Licence: http://opensource.org/licenses/MIT (See also the end of
-# this file)
+# Licence: MIT (http://opensource.org/licenses/MIT)
 #
 # xwindow-switcher:
 #
@@ -58,29 +57,3 @@ choice="${choice:1:(-2)}"
 # desktops.
 wmctrl -i -a $choice
 
-
-# Note: I didn't specify the copyright correctly below. I don't care.
-#
-# =======================================================================
-#
-# The MIT License (MIT)
-
-# Copyright (c) <year> <copyright holders>
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
